ca67af4a7b764f4e57704c268b0cac6c8a756e9b
Chelsea Tarantine <asiajonesCB5@MacBook-Air-29.local>
2019-08-01 18:08:29 -0400

    commit 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,16 @@
+//square that changes when color selected
+const brush = document.querySelector(".brush")
 
+//set color button
+const setColor = document.querySelector("#set-color")
+
+//input field
+const colorField = document.querySelector("#color-field")
+
+
+setColor.addEventListener("click",(event) => {
+    event.preventDefault();
+//check if button works
+//console.log("hello")
+brush.style.backgroundColor = colorField.value;
+})
